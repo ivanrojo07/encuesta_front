ed3bdb98fc81059f6437ffbf22a71045e749c1d0
Ivan Rojo <ivanrojo07@gmail.com>
2021-11-17 11:43:59 -0600

    Hot reload in webpack

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,11 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     entry: path.join(__dirname, 'src', 'index.js'),
+    devtool: 'inline-source-map',
+    devServer: {
+        static: './dist',
+        hot: true,
+    },
     output: {
         path: path.resolve(__dirname, 'dist'),
     },
